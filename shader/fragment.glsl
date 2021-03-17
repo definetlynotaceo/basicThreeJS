c8a171d76e5c47561004e4f32c0e2475addaa8e4
Erik Jung <erik.jung@5pectre.com>
2021-03-18 00:01:31 +0100

    new Build. Fixed MimeType Errors

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -1,13 +1,5 @@
-uniform float time;
-uniform float progress;
-uniform sampler2D image;
-uniform sampler2D displacement;
-uniform vec3 resolution;
-
 varying vec2 vUv;
-varying vec3 vPosition;
-
-void main(){
-  vec4 color = texture2D(image,vUv);
-  gl_FragColor = color ;
+void main()	{
+	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+	gl_FragColor = vec4(vUv,0.0,1.);
 }
--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,14 +1,5 @@
-uniform float time;
 varying vec2 vUv;
-
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-
-uniform vec2 pixels;
-uniform vec2 uvRate1;
-
-void main(){
+void main() {
   vUv = uv;
-
-  // gl_position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
